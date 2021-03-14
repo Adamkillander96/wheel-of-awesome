daa72a58a0f02e949b9931eb7829e162857d4a51
Adam Killander <34036252+Adamkillander96@users.noreply.github.com>
2021-03-14 23:12:44 +0100

    Turned the wheel into template
    You can now use custom wedges to set the price amounts and such. This need iterations. I consider using icons instead of text to make the wedges look better.

---
+++ src/data/rules/wheel.js
@@ -0,0 +1,124 @@
+export const wheel = {
+	wedges: [
+		{
+			name: 'special',
+			type: 'bankrupt',
+			text: 'Bankrupt'
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 500
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 225
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 425
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 300
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 175
+		},
+		{
+			name: 'special',
+			type: 'buy_vowel',
+			text: 'Buy a vowel'
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 125
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 400
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 200
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 325
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 100
+		},
+		{
+			name: 'special',
+			type: 'lose_turn',
+			text: 'Lose a turn'
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 450
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 300
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 200
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 375
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 100
+		},
+		{
+			name: 'special',
+			type: 'free_spin',
+			text: 'Free spin'
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 150
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 250
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 350
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 275
+		},
+		{
+			name: 'money',
+			type: 'price',
+			amount: 50
+		}
+	]
+}
