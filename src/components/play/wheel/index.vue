264d975baa32e880e90a124c8e47ac593a06d935
Adam Killander <34036252+Adamkillander96@users.noreply.github.com>
2021-03-14 23:13:17 +0100

    In new file

---
+++ src/components/play/wheel/index.vue
@@ -0,0 +1,565 @@
+<template>
+	<svg viewBox="0 0 513 513" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g id="wheel_of_awesome" clip-path="url(#clip0)">
+			<g id="wheel background">
+				<path
+					id="background"
+					d="M0.416511 256.318C0.408449 289.976 7.031 323.307 19.9059 354.405C32.7809 385.504 51.656 413.762 75.4533 437.565C99.2505 461.368 127.504 480.25 158.599 493.132C189.695 506.014 223.024 512.645 256.682 512.645C290.34 512.645 323.669 506.014 354.765 493.132C385.86 480.25 414.113 461.368 437.911 437.565C461.708 413.762 480.583 385.504 493.458 354.405C506.333 323.307 512.956 289.977 512.948 256.318C512.956 222.66 506.333 189.329 493.458 158.231C480.583 127.132 461.708 98.8744 437.911 75.0714C414.114 51.2685 385.86 32.3866 354.765 19.5042C323.669 6.62182 290.34 -0.00870092 256.682 -0.00870605C223.024 -0.00871118 189.695 6.6218 158.599 19.5042C127.504 32.3865 99.2506 51.2684 75.4533 75.0714C51.656 98.8743 32.7809 127.132 19.906 158.231C7.03102 189.329 0.408459 222.66 0.416511 256.318Z"
+				/>
+				<path
+					id="inside-background"
+					d="M6.63568 256.318C6.63565 394.344 118.656 506.364 256.682 506.365C394.708 506.365 506.729 394.344 506.729 256.318C506.729 118.292 394.708 6.27158 256.682 6.27155C118.657 6.27152 6.63572 118.292 6.63568 256.318Z"
+				/>
+				<path
+					id="middle-circle"
+					d="M162.896 256.294C162.896 308.049 204.9 350.053 256.655 350.053C308.409 350.053 350.413 308.049 350.413 256.294C350.413 204.54 308.409 162.536 256.655 162.536C204.9 162.536 162.896 204.54 162.896 256.294Z"
+				/>
+			</g>
+
+			<g class="wedges">
+				<g class="wedge" id="special_1">
+					<path
+						class="background"
+						d="M281.519 165.281C273.411 163.11 265.261 162.064 257.246 162.062L257.214 5.80291C280.218 5.79824 299.723 8.34641 321.945 14.2959L281.519 165.281Z"
+					/>
+					<text
+						class="text"
+						transform="translate(256.803 5.53735) rotate(7.04262)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="word" x="20.8828" y="30.3047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_1_1">
+					<path
+						class="background"
+						d="M304.003 174.628C297.06 170.625 289.503 167.454 281.397 165.284L321.822 14.2987C344.044 20.2481 362.182 27.7714 382.106 39.2695L304.003 174.628Z"
+					/>
+					<text
+						class="text"
+						transform="translate(322.068 14.3194) rotate(22.5337)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_1_2">
+					<path
+						class="background"
+						d="M323.471 189.537C317.801 183.874 311.322 178.855 304.054 174.66L382.18 39.2829C402.104 50.781 417.677 62.7422 433.947 79.0055L323.471 189.537Z"
+					/>
+					<text
+						class="text"
+						transform="translate(382.052 39.0775) rotate(37.789)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_1_3">
+					<path
+						class="background"
+						d="M338.276 208.9C334.264 201.962 329.305 195.437 323.37 189.504L433.872 78.9603C450.142 95.2235 462.088 110.808 473.594 130.728L338.276 208.9Z"
+					/>
+					<text
+						class="text"
+						transform="translate(432.943 79.625) rotate(51.7895)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_1_4">
+					<path
+						class="background"
+						d="M347.581 231.39C345.503 223.65 342.401 216.064 338.204 208.797L473.551 130.619C485.058 150.539 492.563 168.684 498.521 190.904L347.581 231.39Z"
+					/>
+					<text
+						class="text"
+						transform="translate(473.455 130.44) rotate(67.6975)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_1_5">
+					<path
+						class="background"
+						d="M350.916 255.469C350.911 247.455 349.879 239.324 347.705 231.218L498.675 190.736C504.633 212.955 507.187 232.424 507.191 255.429L350.916 255.469Z"
+					/>
+					<text
+						class="text"
+						transform="translate(498.536 191.181) rotate(82.3228)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="special_2">
+					<path
+						class="background"
+						d="M347.693 279.891C349.865 271.783 350.913 263.633 350.916 255.618L507.175 255.612C507.176 278.616 504.625 298.12 498.672 320.341L347.693 279.891Z"
+					/>
+					<text
+						class="text"
+						transform="translate(507.441 255.202) rotate(97.052)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="word" x="20.8828" y="30.3047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_2_1">
+					<path
+						class="background"
+						d="M338.494 302.414C342.498 295.472 345.67 287.915 347.842 279.809L498.82 320.26C492.867 342.481 485.341 360.617 473.84 380.54L338.494 302.414Z"
+					/>
+					<text
+						class="text"
+						transform="translate(498.8 320.506) rotate(112.543)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_2_2">
+					<path
+						class="background"
+						d="M323.716 321.959C329.381 316.29 334.401 309.812 338.596 302.544L473.961 380.692C462.459 400.615 450.496 416.185 434.23 432.452L323.716 321.959Z"
+					/>
+					<text
+						class="text"
+						transform="translate(474.166 380.565) rotate(127.798)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_2_3">
+					<path
+						class="background"
+						d="M304.464 336.875C311.402 332.865 317.928 327.907 323.862 321.973L434.388 432.493C418.122 448.76 402.536 460.703 382.614 472.206L304.464 336.875Z"
+					/>
+					<text
+						class="text"
+						transform="translate(433.723 431.564) rotate(141.799)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_2_4">
+					<path
+						class="background"
+						d="M282.052 346.31C289.793 344.232 297.379 341.132 304.647 336.936L382.802 472.296C362.88 483.799 344.734 491.302 322.513 497.256L282.052 346.31Z"
+					/>
+					<text
+						class="text"
+						transform="translate(382.981 472.2) rotate(157.707)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_2_5">
+					<path
+						class="background"
+						d="M258.014 349.793C266.028 349.789 274.159 348.758 282.265 346.586L322.723 497.562C300.502 503.517 281.033 506.067 258.028 506.068L258.014 349.793Z"
+					/>
+					<text
+						class="text"
+						transform="translate(322.277 497.423) rotate(172.332)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="special_3">
+					<path
+						class="background"
+						d="M233.617 346.758C241.725 348.928 249.875 349.975 257.89 349.976L257.922 506.235C234.918 506.24 215.413 503.692 193.191 497.742L233.617 346.758Z"
+					/>
+					<text
+						class="text"
+						transform="translate(258.332 506.501) rotate(-172.957)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="word" x="20.8828" y="30.3047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_3_1">
+					<path
+						class="background"
+						d="M211.133 337.41C218.076 341.413 225.633 344.584 233.739 346.755L193.314 497.74C171.092 491.79 152.954 484.267 133.03 472.769L211.133 337.41Z"
+					/>
+					<text
+						class="text"
+						transform="translate(193.068 497.719) rotate(-157.466)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_3_2">
+					<path
+						class="background"
+						d="M191.665 322.501C197.335 328.165 203.814 333.184 211.083 337.378L132.957 472.755C113.032 461.257 97.4595 449.296 81.1897 433.033L191.665 322.501Z"
+					/>
+					<text
+						class="text"
+						transform="translate(133.084 472.961) rotate(-142.211)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_3_3">
+					<path
+						class="background"
+						d="M176.861 303.138C180.872 310.076 185.831 316.601 191.766 322.534L81.2641 433.078C64.9942 416.815 53.0483 401.23 41.5421 381.31L176.861 303.138Z"
+					/>
+					<text
+						class="text"
+						transform="translate(82.1927 432.413) rotate(-128.211)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_3_4">
+					<path
+						class="background"
+						d="M167.555 280.649C169.633 288.389 172.735 295.975 176.933 303.242L41.5846 381.419C30.0784 361.499 22.573 343.354 16.6145 321.135L167.555 280.649Z"
+					/>
+					<text
+						class="text"
+						transform="translate(41.681 381.599) rotate(-112.303)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_3_5">
+					<path
+						class="background"
+						d="M164.22 256.569C164.225 264.583 165.257 272.713 167.431 280.819L16.4611 321.302C10.5026 299.083 7.94924 279.613 7.94456 256.609L164.22 256.569Z"
+					/>
+					<text
+						class="text"
+						transform="translate(16.6 320.856) rotate(-97.6772)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="special_4">
+					<path
+						class="background"
+						d="M167.443 232.148C165.271 240.255 164.223 248.405 164.22 256.42L7.96074 256.426C7.95985 233.422 10.5112 213.918 16.4643 191.697L167.443 232.148Z"
+					/>
+					<text
+						class="text"
+						transform="translate(7.69513 256.837) rotate(-82.948)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="word" x="20.8828" y="30.3047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_4_1">
+					<path
+						class="background"
+						d="M176.642 209.624C172.638 216.566 169.466 224.122 167.294 232.229L16.3156 191.778C22.2687 169.557 29.7949 151.421 41.2964 131.498L176.642 209.624Z"
+					/>
+					<text
+						class="text"
+						transform="translate(16.3364 191.532) rotate(-67.4569)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_4_2">
+					<path
+						class="background"
+						d="M191.42 190.078C185.755 195.748 180.735 202.226 176.539 209.494L41.175 131.345C52.6764 111.423 64.6402 95.8523 80.9062 79.5851L191.42 190.078Z"
+					/>
+					<text
+						class="text"
+						transform="translate(40.9697 131.473) rotate(-52.2015)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_4_3">
+					<path
+						class="background"
+						d="M210.672 175.162C203.734 179.172 197.208 184.13 191.274 190.065L80.7483 79.5443C97.0142 63.277 112.6 51.3338 132.522 39.8308L210.672 175.162Z"
+					/>
+					<text
+						class="text"
+						transform="translate(81.4128 80.473) rotate(-38.2011)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_4_4">
+					<path
+						class="background"
+						d="M233.083 165.727C225.343 167.805 217.756 170.905 210.489 175.101L132.334 39.7407C152.256 28.2377 170.402 20.7354 192.622 14.7805L233.083 165.727Z"
+					/>
+					<text
+						class="text"
+						transform="translate(132.154 39.837) rotate(-22.2931)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+
+				<g class="wedge" id="price_money_4_5">
+					<path
+						class="background"
+						d="M257.122 162.244C249.108 162.247 240.977 163.278 232.871 165.451L192.413 14.4745C214.634 8.51959 234.103 5.96943 257.108 5.96854L257.122 162.244Z"
+					/>
+					<text
+						class="text"
+						transform="translate(192.859 14.6135) rotate(-7.66782)"
+						xml:space="preserve"
+						style="white-space: pre"
+						font-family="Roboto"
+						font-size="36"
+						letter-spacing="0em"
+					>
+						<tspan class="number" x="22.5176" y="27.8047"></tspan>
+						<tspan class="number" x="22.5176" y="58.8047"></tspan>
+						<tspan class="number" x="22.5176" y="89.8047"></tspan>
+					</text>
+				</g>
+			</g>
+		</g>
+		<defs>
+			<clipPath id="clip0">
+				<rect width="513" height="513" transform="matrix(-1 0 0 1 513 0)" />
+			</clipPath>
+		</defs>
+	</svg>
+</template>
+
+<script>
+import { wheel } from '../../../data/rules/wheel'
+export default {
+	name: 'Wheel',
+	data: () => ({
+		wheel
+	}),
+	mounted() {
+		const { wedges } = this.wheel
+		wedges.forEach(({ name, type, amount, text = '' }, index) => {
+			const wedge = document.querySelector('.wedges').children[index]
+			if (text) {
+				wedge.dataset.special = type
+				wedge.querySelector('.word').innerHTML = text
+				return
+			}
+			const price = amount + ''
+			price.split('').forEach((number, index) => {
+				const css_index = (index += 1)
+				wedge.querySelector(
+					`.number:nth-child(${css_index})`
+				).innerHTML = number
+			})
+		})
+	}
+}
+</script>
+
+<style lang="scss">
+$wedges: (
+	'first' '2' var(--pink),
+	'second' '3' var(--green),
+	'third' '4' var(--orange),
+	'fourth' '5' var(--blue),
+	'fifth' '0' var(--yellow)
+);
+
+$special_wedges: (
+	'bankrupt' var(--black),
+	'vowel' var(--purple),
+	'lose_turn' var(--red),
+	'free_spin' var(--free)
+);
+
+.wedge {
+	.text {
+		fill: var(--white);
+	}
+	@each $name, $index, $color in $wedges {
+		&:nth-child(6n + #{$index}) .background {
+			fill: $color;
+		}
+	}
+	@each $name, $color in $special_wedges {
+		&[data-special*='#{$name}'] .background {
+			fill: $color;
+		}
+	}
+	&:hover {
+		.background {
+			filter: brightness(0.75);
+		}
+	}
+}
+</style>
